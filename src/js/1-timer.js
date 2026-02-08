b285c214a4ab091ba25cddba5f205da72d4c04d9
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-02-08 18:03:27 +0100

    push

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -87,5 +87,3 @@ function updateTimer(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, "0");
 }
-
-
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,36 +1,37 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const btn = document.querySelector('[type="submit"]');
+const form = document.querySelector('.form');
 
 
-btn.addEventListener('click',(event) => {
-    const radio = document.querySelector('[name="state"]:checked');
+form.addEventListener('submit',(event) => {
+    
     event.preventDefault();
 
-    const delay = document.querySelector("input").value;
+    const radio = document.querySelector('[name="state"]:checked');
+    const delay = document.querySelector('input[name="state"]').value;
 
     const promise  = new Promise((resolve,reject) =>{
     setTimeout(() =>{
     if(radio.value == 'fulfilled'){
-        resolve(`✅ Fulfilled promise in ${delay}ms`);
+        resolve(delay);
     }
     if(radio.value == 'rejected'){
-        reject(`❌ Rejected promise in ${delay}ms`);
+        reject(delay);
     }
     }
     ,delay)
     })
     promise
-        .then(value =>{
+        .then(delay =>{
         iziToast.success({
-            message: value,
+            message: `✅ Fulfilled promise in ${delay}ms`,
             position: "topRight",
             });
         })
-        .catch(value => {
+        .catch(delay => {
             iziToast.error({
-                message: value,
+                message: `❌ Rejected promise in ${delay}ms`,
                 position: "topRight",
                 });
         })
